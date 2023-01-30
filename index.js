7992b934a65aec2e389cc28d56ec45a2e009ed45
Charles Oliveira <ch4rle2.fh@gmail.com>
2023-01-30 17:02:34 -0300

    initial commit

---
+++ index.js
@@ -0,0 +1,34 @@
+const jokeEl = document.querySelector('#joke')
+const btn = document.querySelector('#btn')
+
+const apiKey = 'FXqdRLDPstXS0/HP171gxA==DdE1bXmWUc8RhXZI'
+const apiURL = 'https://api.api-ninjas.com/v1/dadjokes?limit=1'
+
+const options = {
+    method: 'GET',
+    headers: {
+        'X-Api-Key': apiKey
+    }
+}
+
+async function generateJoke() {
+    try {
+        jokeEl.innerText = 'Updating...'
+        btn.disabled = true
+        const response = await fetch(apiURL, options) 
+        const data = await response.json()
+        btn.disabled = false
+        btn.innerText = 'Tell me a joke'
+
+        jokeEl.innerText = data[0].joke
+    } catch (error) {
+        btn.disabled = false
+        btn.innerText = 'Tell me a joke'
+        jokeEl.innerText = `An ${error} happened, try again later`
+
+        // console.log(error)
+    }
+   
+}
+
+btn.addEventListener('click', generateJoke)
